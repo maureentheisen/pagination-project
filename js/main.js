e7a685b302c0c9c8f618f7845cca4edabb882209
Maureen Theisen <mtheisen@replacements.com>
2018-07-23 16:21:22 -0400

    fixing pager

--- js/main.js
+++ js/main.js
@@ -1,14 +1,17 @@
 //selects the list items with class .student-item
 const students = document.querySelectorAll('.student-item');
-
 const perPage = 10;
 const numStudents = students.length;
-//function that takes a pageNumber and displays all the students, 10 per page
+const totalPages = Math.ceil(students/perPage);
+
 function showPage(pageNumber, students, perPage) {
 
-    for (var i = 0; i < students.length; i++) {
+    maxItems = pageNumber * perPage - 1;
+    minItems = (maxItems  + 1) - perPage;
+
+    for (var i = 0; i < students.length - 1; i++) {
         students[i].style.display = 'none';
-        if (i >= 0 && i <= (perPage - 1)) {
+        if (i >= minItems && i <= (maxItems - 1)) {
            students[i].style.display = 'block';
         }
     }
@@ -18,16 +21,24 @@ showPage(3, students, perPage);
 
 
 function appendPageLinks(students, perPage) {
-    const totalPages = Math.ceil(students/perPage);
+    const totalPages = Math.ceil(students / perPage);
     const pagination = document.getElementById('pagination');
     const ul = document.createElement("ul");
+
+
     pagination.appendChild(ul);
 
-    for(var j=1; j<=totalPages; j++){
-       var li = document.createElement("li");
+    for (var j = 1; j <= totalPages; j++) {
+        var li = document.createElement("li");
         ul.appendChild(li);
-        li.innerHTML = '<a href="#">' +  j  + '</a>';
+        li.innerHTML = '<a href="#">' + j + '</a>';
+
     }
-}
+
+
+};
+
 
 appendPageLinks(numStudents, perPage);
+
+
