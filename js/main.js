553565664ca813d3ff5caf549a238c5bf5c88fa9
Maureen Theisen <mtheisen@replacements.com>
2018-07-23 13:53:56 -0400

    adding pagination

--- js/main.js
+++ js/main.js
@@ -1,16 +1,33 @@
+//selects the list items with class .student-item
+const students = document.querySelectorAll('.student-item');
 
-function showPage(perPage, pageNumber) {
-  const student = $('.student-item')
-  const students = student.length;
+const perPage = 10;
+const numStudents = students.length;
+//function that takes a pageNumber and displays all the students, 10 per page
+function showPage(pageNumber, students, perPage) {
 
-  student.hide();
-
-    $( student ).each(function(index) {
-      if(index < perPage){
-        $( this ).show();
+    for (var i = 0; i < students.length; i++) {
+        students[i].style.display = 'none';
+        if (i >= 0 && i <= (perPage - 1)) {
+           students[i].style.display = 'block';
+        }
     }
-    });
-  };
+}
+
+showPage(3, students, perPage);
 
 
-showPage(5, 2);
+function appendPageLinks(students, perPage) {
+    const totalPages = Math.ceil(students/perPage);
+    const pagination = document.getElementById('pagination');
+    const ul = document.createElement("ul");
+    pagination.appendChild(ul);
+
+    for(var j=1; j<=totalPages; j++){
+       var li = document.createElement("li");
+        ul.appendChild(li);
+        li.innerHTML = '<a href="#">' +  j  + '</a>';
+    }
+}
+
+appendPageLinks(numStudents, perPage);
